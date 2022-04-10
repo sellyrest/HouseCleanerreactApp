d9524f0740a25a8b88afb25015efe5ce297e4575
Selly Resty Wijayanti <76523501+sellyrest@users.noreply.github.com>
2022-04-10 15:04:32 +0700

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Appbar from "../components/Appbar";
+import Content from "../components/Content";
+import Footer from "../components/Footer";
+
+function Home() {
+    return (
+        <div className="App">
+            <Appbar />
+            <Content />
+            <Footer />
+
+        </div>
+    );
+}
+
+export default Home;
